a501bf427028a07f503c5c500622eb3b874b14e9
DanilProV-ctrl <ProVDanil2019@yandex.ru>
2019-11-23 23:53:11 +1000

    Favorite-s

---
+++ JuStYm.py
@@ -0,0 +1,27 @@
+what = input("что выбрать?(+,-)")
+a = float(input("1:"))
+b = float(input("2:"))
+if what == "+":
+    c = a + b
+    print("Результат " + str(c))
+elif what == "-":
+    c = a - b
+    print("Результат " + str(c))
+else:
+    print("nothing")
+
+print("thanks for watching")
+print("Сколько вам лет?")
+age = float(input("age:"))
+if age>18:
+    print("okey")
+elif age<18:
+    print("lox")
+if age == 18:
+    print("Ohhh")
+
+'''O
+K
+E
+Y'''
+    
